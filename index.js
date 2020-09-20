e4f0abd68678ea5a4afe8cc681cbdd62349c766c
Antti Solonen <antti.solonen@leanheat.com>
2020-09-20 15:50:07 +0300

    puhelinluettelo ja tietokanta stepit 3-4

--- index.js
+++ index.js
@@ -42,16 +42,22 @@ app.get('/info', (req, res) => {
             <p>${new Date()}</p>`)
 })
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id).then(person => {
     res.json(person)
   })
+  .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-  res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+  //const id = Number(req.params.id)
+  //persons = persons.filter(person => person.id !== id)
+  //res.status(204).end()
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res) => {
@@ -85,6 +91,23 @@ app.post('/api/persons', (req, res) => {
     res.json(savedPerson)
   })
 })
+
+// unknown endpoint handling
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+// error handling
+const errorHandler = (error, request, response, next) => {
+  console.log('IN ERROR HANDLER')
+  console.log(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {

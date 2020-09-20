cadf86e22f7a694bb0084dddfbedaa59585a09e9
Antti Solonen <antti.solonen@leanheat.com>
2020-09-20 16:08:18 +0300

    puhelinluettelo ja tietokanta stepit 5-6

--- index.js
+++ index.js
@@ -19,14 +19,6 @@ app.use(express.static('build'))
 app.use(express.json()) 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postdata'))
 
-let persons = [
-  { name: 'Arto Hellas', number: '040-123456', id: 1 },
-  { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-  { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-  { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  { name: 'Dirl', number: '3', id: 5 }
-]
-
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
@@ -38,8 +30,10 @@ app.get('/api/persons', (req, res) => {
 })
 
 app.get('/info', (req, res) => {
-  res.send(`<p>Phonebook has info for ${persons.length} people</p>
-            <p>${new Date()}</p>`)
+  Person.find({}).then(people => {
+    res.send(`<p>Phonebook has info for ${people.length} people</p>
+              <p>${new Date()}</p>`)
+  })
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
@@ -50,9 +44,6 @@ app.get('/api/persons/:id', (req, res, next) => {
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-  //const id = Number(req.params.id)
-  //persons = persons.filter(person => person.id !== id)
-  //res.status(204).end()
   Person.findByIdAndRemove(req.params.id)
     .then(result => {
       res.status(204).end()
@@ -74,12 +65,6 @@ app.post('/api/persons', (req, res) => {
       error: 'number missing' 
     })
   }
-  // check if name already exists
-  if (persons.map(p => p.name).includes(body.name)) {
-    return res.status(400).json({ 
-      error: 'name must be unique' 
-    })
-  }
 
   const person = new Person({
     name: body.name,
@@ -87,9 +72,23 @@ app.post('/api/persons', (req, res) => {
   })
 
   person.save().then(savedPerson => {
-    persons = persons.concat(savedPerson)
     res.json(savedPerson)
   })
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, {new: true})
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 // unknown endpoint handling
